65b418701dbf4007d4a41fa4301c18a51c305944
amanrawt03 <amanrawat3021@gmail.com>
2024-06-30 20:01:04 +0530

    5-ui design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,25 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Sidebar from "./components/sidebar";
-import Rightbar from "./components/rightbar";
-import HomePage from "./pages/homePage";
-import TweetListProvider from "./store/tweets-store";
-import SignUp from "./pages/signUp";
-import Login from "./pages/login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import EnterPage from "./pages/enterPage";
-import LogoutModal from "./components/logout";
+import {Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignupPage from "./pages/auth/signup/SignupPage";
+import Sidebar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
-  const [modal, setModal] = useState(false);
-
-  const toggleModal = () => {
-    setModal(!modal);
-  };
-
-  return (
-    <BrowserRouter>
-      <TweetListProvider>
-        <Routes>
-          <Route exact path="/" element={<EnterPage />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="*"
-            element={
-              <>
-                <LogoutModal modal={modal} toggleModal={toggleModal} />
-                <Sidebar toggleModal={toggleModal} />
-                <HomePage />
-                <Rightbar />
-              </>
-            }
-          />
-        </Routes>
-      </TweetListProvider>
-    </BrowserRouter>
-  );
+  return (  
+  <div className='flex max-w-6xl mx-auto'>
+    <Sidebar/>
+  <Routes>
+    <Route path = "/" element = {<HomePage/>}/>
+    <Route path = "/login" element = {<LoginPage/>}/>
+    <Route path = "/signup" element = {<SignupPage/>} />
+    <Route path = "/notifications" element = {<NotificationPage/>} />
+    <Route path = "/profile/:username" element = {<ProfilePage/>} />
+  </Routes>
+  <RightPanel/>
+  </div>)
 }
 
 export default App;
